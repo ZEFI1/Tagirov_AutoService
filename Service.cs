a3e2f6e3edb34569ab0f89e3300324e4a4b34184
renatakhabirva <renatahabirovka49@gmail.com>
2025-10-01 13:58:02 +0500

    5ЛАБА

--- Service.cs
+++ Service.cs
@@ -28,7 +28,7 @@ namespace WpfApp1
         public decimal Cost { get; set; }
         public double Discount { get; set; }
         public string Description { get; set; }
-    
+     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ClientService> ClientService { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
